775cebba3463115bd60fced2e2efe2f60fbb0b9f
KoolKid <93000739+ItzKoolKid96@users.noreply.github.com>
2022-09-25 16:38:22 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,14 @@
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
 echo "  Welcome to KoolCDN Installer Script"
 echo "With this script you can install KoolCDN"
 echo "         On your ubuntu server"
