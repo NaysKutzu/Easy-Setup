b001b3ffff7a49f3562f9644a953b9203c070e0f
KoolKid <93000739+ItzKoolKid96@users.noreply.github.com>
2022-10-24 18:02:26 +0200

    Done

--- script.sh
+++ script.sh
@@ -4,13 +4,6 @@ if [[ $EUID -ne 0 ]]; then
   echo "* This script must be executed with root privileges (sudo)." 1>&2
   exit 1
 fi
-
-# check for curl
-if ! [ -x "$(command -v curl)" ]; then
-  echo "* curl is required in order for this script to work."
-  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
-  exit 1
-fi
 echo "      Welcome to KoolSetup Script"
 echo "With this script you can setup your server"
 echo "         On your ubuntu server"
